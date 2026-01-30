c88686cdf40298300e4db48bf5ce5ff94b60df8d
msugirbay <msugirbay@inbox.ru>
2026-01-30 16:20:40 +0500

    Validator.java

--- src/dormitory/utils/Validator.java
+++ src/dormitory/utils/Validator.java
@@ -1,4 +1,19 @@
 package dormitory.utils;
 
+import java.util.regex.Pattern;
+
 public class Validator {
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@(.+)$");
+
+    public static boolean isValidEmail(String email) {
+        return email != null && EMAIL_PATTERN.matcher(email).matches();
+    }
+
+    public static boolean isValidPassword(String password) {
+        return password != null && password.length() >= 6;
+    }
+
+    public static boolean isPositive(double number) {
+        return number > 0;
+    }
 }
